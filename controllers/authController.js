578bb71a9219ba4ab72ec6f373824eaf836ec0e6
Vicky bisht <v.bisht.unic@gmail.com>
2025-07-25 13:43:25 +0530

    fix: updated cookie options for mobile login support

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,8 +30,8 @@ export const handleRegisterUser = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production", // only HTTPS in production
-            sameSite: "strict",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
         res.status(201).json({
